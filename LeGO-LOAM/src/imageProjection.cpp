3ea93b4445a1930b1b9064b4b982c7c1e5925a40
baptval <61832719+robot54@users.noreply.github.com>
2020-05-18 10:31:46 +0200

    Update imageProjection.cpp

--- LeGO-LOAM/src/imageProjection.cpp
+++ LeGO-LOAM/src/imageProjection.cpp
@@ -26,6 +26,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#define NDEBUG // robot54: needed for speed
 #include <boost/circular_buffer.hpp>
 #include "imageProjection.h"
 
